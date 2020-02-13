6545e9c4107bc2a9ec51472a8dd13f4219c3cdb3
neil.garner <neil.garner@autotrader.co.uk>
2020-02-13 14:37:07 +0000

    Change to port listening

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ const server = new ApolloServer({
   resolvers
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
+server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
 });
